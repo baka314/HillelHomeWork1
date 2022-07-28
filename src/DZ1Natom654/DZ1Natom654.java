96b4ead813a6c780ef37f7c06d42779df141b3be
Omelchenko Nataly <natom654@gmail.com>
2022-07-28 15:28:26 +0200

    my changes3 commit

---
+++ src/DZ1Natom654/DZ1Natom654.java
@@ -0,0 +1,9 @@
+package DZ1Natom654;
+
+public class DZ1Natom654 {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("Hello EugeniyaDzhu");
+        System.out.println("Hello Java Pro");
+    }
+}
