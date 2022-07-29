aaa66043748ca6bec7d1bc54ceca976385370aaf
Darvi <hunter18111982@gmail.com>
2022-07-29 19:02:34 +0300

    hw1_commit

---
+++ src/com/hillel/ozerovdmytro/OzerovDmytro.java
@@ -0,0 +1,8 @@
+package com.hillel.ozerovdmytro;
+
+public class OzerovDmytro {
+    public static void main(String[] args) {
+        String text = "Hello World";
+        System.out.println(text);
+    }
+}
