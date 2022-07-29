1a071592b6e7aa5d935c2ed9c74a484ddb580d9b
Darvi <hunter18111982@gmail.com>
2022-07-29 18:50:48 +0300

    hw1_commit

--- src/com/hillel/Main.java
+++ src/com/hillel/Main.java
@@ -1,9 +1,12 @@
 package com.hillel;
 
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Java");
         System.out.println("Hello EugeniyaDzhu");
         System.out.println("Hello Java Pro");
+        System.out.println("Hello World");
     }
 }
