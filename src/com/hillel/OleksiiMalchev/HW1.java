3eea938133e8ac9bb4f22c5ef4e59c13aae9843e
OleksiiMalchev <oleksiimalchev@gmail.com>
2022-07-30 10:38:50 +0300

    HW1 corrections have been made according to the remark

--- src/com/hillel/OleksiiMalchev/HW1.java
+++ src/com/hillel/OleksiiMalchev/HW1.java
@@ -2,6 +2,8 @@ package com.hillel.OleksiiMalchev;
 
 public class HW1 {
     public static void main(String[] args) {
-        System.out.println("My Home Work 1");
+
+        String message = "My Home Work 1";
+        System.out.println(message);
     }
 }
