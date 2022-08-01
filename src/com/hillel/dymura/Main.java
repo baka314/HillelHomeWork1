d261d7264e70ef52560955835e9f41769e843cf2
slay4u <galaxy.vd123@gmail.com>
2022-08-01 21:55:37 +0300

    commit

---
+++ src/com/hillel/dymura/Main.java
@@ -0,0 +1,9 @@
+package com.hillel.dymura;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+        System.out.println("Hello EugeniyaDzhu");
+        System.out.println("Hello Java Pro");
+    }
+}
