d48359d85e4ef47b3b016fbc852dc3f8936da47a
baka314 <95147513+baka314@users.noreply.github.com>
2022-08-02 13:09:21 +0300

    Update Main.java

--- src/com/hillel/Main.java
+++ src/com/hillel/Main.java
@@ -4,6 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Java");
         System.out.println("Hello EugeniyaDzhu");
+        System.out.println("_______________________________");
         System.out.println("Hello Java Pro");
+        
     }
 }
